18ce5d322564e8910a50123a2b010b3d24bc71a1
Caroline <liardcar@gmail.com>
2022-07-21 15:40:14 +0200

    chore(06--angular): people httpservice

--- 06--angular/about-the-universe/src/app/app.module.ts
+++ 06--angular/about-the-universe/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { IndexShowPlanetsComponent } from './index-show-planets/index-show-planets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    IndexShowPlanetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 06--angular/about-the-universe/src/app/homepage/homepage.component.ts
+++ 06--angular/about-the-universe/src/app/homepage/homepage.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
+
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  SWAPI_PEOPLE_URL = 'https://swapi.dev/api/people/1/';
+  character: any = [];
+
+  // Injecter le service
+  constructor(private httpService: HttpClient) { }
 
   ngOnInit(): void {
-  }
+    this.getCharacter();
+  };
 
-}
+  getCharacter() {
+    this.httpService
+    .get(this.SWAPI_PEOPLE_URL)
+    .subscribe( data => {
+      this.character = data;
+    })
+  }
+};
